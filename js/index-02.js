6b82ed76acedbf56075d8cfdaa53d00f407c40cc
Vozniuk Volodymyr <vvvozniuk@gmail.com>
2022-01-08 23:26:46 +0200

    Tasks 1-20

--- js/index-02.js
+++ js/index-02.js
@@ -1,7 +1,7 @@
-// ! Функции и массивы
+// ! Модуль 2: Функции и массивы
 
 /*
- * 1. Напиши скрипт поиска самого маленького числа в массиве. Код должен работать для любого массива чисел. Используй цикл для решения задачи.
+ * Напиши скрипт поиска самого маленького числа в массиве. Код должен работать для любого массива чисел. Используй цикл для решения задачи.
  */
 
 // const numbers = [2, 17, 94, 1, 23, -37, 0, -5, -125];
@@ -9,25 +9,25 @@
 // function getSmallestNumber() {
 //   return Math.min(...numbers);
 // }
-// console.log('smallestNumber:', getSmallestNumber());
+// console.log("smallestNumber:", getSmallestNumber());
 
 /*
- * 2. Напиши функцию findLogin(allLogins, login) для поиска логина
+ * Напиши функцию findLogin(allLogins, login) для поиска логина
  * - Если логина нет, вывести сообщение 'Пользователь [логин] не найден.'
  * - Если нашли логин, вывести сообщение 'Пользователь [логин] найден.'
  */
 
 // const logins = ['m4ngoDoge', 'k1widab3st', 'poly1scute', 'aj4xth3m4n'];
 
-// // const findLogin = function (allLogins, loginToFind) {
-// //   for (const login of allLogins) {
-// //     if (login === loginToFind) {
-// //       return `Пользователь ${loginToFind} найден.`;
-// //     }
-// //   }
+// const findLogin = function (allLogins, loginToFind) {
+//   for (const login of allLogins) {
+//     if (login === loginToFind) {
+//       return `Пользователь ${loginToFind} найден.`;
+//     }
+//   }
 
-// //   return `Пользователь ${loginToFind} не найден.`;
-// // };
+//   return `Пользователь ${loginToFind} не найден.`;
+// };
 
 // const findLogin = function (allLogins, loginToFind) {
 //   return allLogins.includes(loginToFind)
@@ -41,7 +41,7 @@
 // console.log(findLogin(logins, 'poly1scute'));
 
 /*
- * 3. Напиши функцию filterNumbers(array [, number1, ...]) которая:
+ * Напиши функцию filterNumbers(array [, number1, ...]) которая:
  * - первым аргументом принимает массив чисел
  * - после первого аргумента может быть произвольное количество других аргументов которые будут числами.
  * - Функция должна вернуть новый массив, в котором будут только те аргументы, начиная со второго,
@@ -67,3 +67,280 @@
 // console.log(filterNumbers([1, 2, 3, 4, 5], 10, 15, 2, 3, 8)); // [2, 3]
 // console.log(filterNumbers([10, 15, 25, 30], 23, 30, 18, 15)); // [30, 15]
 // console.log(filterNumbers([100, 200, 300, 400, 500], 7, 12, 200, 64)); // [200]
+
+// * 1. Запиши условие в инструкции if так, чтобы функция работала правильно.
+
+// function checkAge(age) {
+//   if (age >= 18) {
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+
+// console.log(checkAge(19));
+
+// * 2. Функция checkPassword получает пароль пользователя в параметр password, проверяет его на совпадение с паролем администратора в переменной ADMIN_PASSWORD и возвращает сообщение о результате сравнения Проведи рефакторинг кода функции checkPassword используя паттерн «ранний возврат»:
+// * удали переменную message
+// * удали else
+// * код должен работать так же, как и до оптимизации
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+
+//   if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   }
+
+//   return "Access denied, wrong password!";
+// }
+
+// console.log(checkPassword("jqueryismyjam"));
+
+// * 3. Функция checkStorage проверяет возможность оформления заказа и возвращает сообщение о результате. Она принимает два параметра, значения которых будут задаваться во время её вызова.
+
+// * available - доступное количество товаров на складе
+// * ordered - количество единиц товара в заказе
+// * Проведи рефакторинг кода функции checkStorage используя паттерн «ранний возврат».
+
+// function checkStorage(available, ordered) {
+//   if (ordered === 0) {
+//     console.log("Your order is empty!");
+//     return;
+//   }
+
+//   if (ordered > available) {
+//     console.log("Your order is too large, not enough goods in stock!");
+//     return;
+//   }
+//   console.log("The order is accepted, our manager will contact you");
+// }
+
+// checkStorage(5, 1);
+
+// * Второй вариант (правильный для автопроверки)
+
+// function checkStorage(available, ordered) {
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//   }
+
+//   if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   }
+//   return "The order is accepted, our manager will contact you";
+// }
+
+// console.log(checkStorage(5, 40));
+
+// * 4. Объяви переменную fruits и присвой ей массив фруктов - строк "apple", "plum", "pear" и "orange".
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+// console.log(fruits);
+
+// * 5. Объяви три переменные и присвой каждой из них значение, используя нотацию квадратных скобок.
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[3];
+
+// console.log(firstElement, secondElement, lastElement);
+
+// * 6. Переопредели значения элементов с индексами 1 и 3. Замени "plum" на "peach", а "orange" на "banana".
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// fruits[1] = "peach";
+// fruits[3] = "banana";
+
+// console.log(fruits);
+
+// * 7. Объяви переменную fruitsArrayLength и присвой ей длину массива fruits используя свойство length.
+
+// const fruits = ["apple", "peach", "pear", "banana", "orange"];
+// const fruitsArrayLength = fruits.length;
+
+// console.log(fruitsArrayLength);
+
+// * 8. Объяви две переменные: lastElementIndex - Индекс последнего элемента масcива fruits через длина_массива - 1. lastElement - Значение последнего элемента массива
+
+// const fruits = ["apple", "peach", "pear", "banana", "orange"];
+
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+// console.log(lastElementIndex);
+// console.log(lastElement);
+
+// * 9. Напиши функцию getExtremeElements(array) которая принимает один параметр array - массив элементов произвольной длины. Функция должна возвращать массив из двух элементов - первого и последнего элемента параметра array.
+
+// * Вариант 1
+// function getExtremeElements(...array) {
+//   const firstAndLastElements = [array[0], array[array.length - 1]];
+//   console.log(firstAndLastElements);
+// }
+
+// getExtremeElements("apple", "peach", "pear", "banana", "orange");
+// getExtremeElements(1, 2, 3, 4, 5, 6, 7);
+
+// * Вариант 2
+// function getExtremeElements(array) {
+//   const firstAndLastElements = [array[0], array[array.length - 1]];
+//   return firstAndLastElements;
+// }
+
+// console.log(getExtremeElements(["Earth", "Mars", "Venus"]));
+// console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));
+// console.log(getExtremeElements([1, 2, 3, 4, 5]));
+
+// * 10. Дополни код функции splitMessage(message, delimeter) так, чтобы она возвращала в переменной words результат разделения строки message по разделителю delimeter - массив строк.
+
+// function splitMessage(message, delimeter) {
+//   let words;
+//   words = message.split(delimeter);
+//   return words;
+// }
+
+// console.log(splitMessage("Mango hurries to the train", " "));
+// console.log(splitMessage("Mango", ""));
+// console.log(splitMessage("best_for_week", "_"));
+
+// * 11. Сервису гравировки украшений нужна функция, которая бы автоматически считала цену гравировки, в зависимости от количества слов и цены за слово. Объявлена функция calculateEngravingPrice(message, pricePerWord). Эта функция принимает строку, состоящую из слов разделённых только пробелами (параметр message) и цену гравировки одного слова (параметр pricePerWord). Напиши тело функции, чтобы она возвращала общую стоимость гравировки всех слов в строке.
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//   let totalPrice = message.split(" ").length * pricePerWord;
+//   return totalPrice;
+// }
+
+// console.log(calculateEngravingPrice("Web-development is creative work", 40));
+
+// * 12. Дополни код функции makeStringFromArray(array, delimeter) так, чтобы она возвращала в переменной string результат соединения элементов массива array c разделителем delimeter - строку.
+
+// function makeStringFromArray(array, delimeter) {
+//   let string;
+//   string = array.join(delimeter);
+//   return string;
+// }
+
+// console.log(makeStringFromArray(["M", "a", "n", "g", "o"], ""));
+
+// * 13. Напиши функцию slugify(title) которая принимает заголовок статьи, параметр title, и возвращает slug, созданный из этой строки. Значением параметра title будут строки, слова которых разделены только пробелами. Все символы slug должны быть в нижнем регистре. Все слова slug должна быть разделены тире
+
+// function slugify(title) {
+//   const slug = title.toLowerCase().split(" ").join("-");
+//   return slug;
+// }
+
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+// * 14. Дополни код так, чтобы переменные содержали частичные копии исходного массива fruits. firstTwoEls - массив из первых двух элементов, nonExtremeEls - массив из всех элементов кроме первого и последнего, lastThreeEls - массив из трёх последних элементов
+
+// const fruits = ["apple", "plum", "pear", "orange", "banana"];
+
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, 4);
+// const lastThreeEls = fruits.slice(-3);
+
+// console.log(firstTwoEls);
+// console.log(nonExtremeEls);
+// console.log(lastThreeEls);
+
+// * 15. Дополни код так, чтобы в переменной allClients получился массив всех элементов массивов oldClients и newClients.
+
+// const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// const newClients = ["Peach", "Houston"];
+
+// const allClients = oldClients.concat(newClients);
+
+// console.log(allClients);
+
+// * 16. Напиши функцию makeArray(firstArray, secondArray, maxLength) для создания нового массива со всеми элементами двух исходных firstArray и secondArray. Параметр maxLength содержит максимально допустимую длину нового массива. Если количество элементов нового массива больше maxLength, функция должна вернуть копию массива длиной maxLength элементов. В противном случае функция должна вернуть новый массив целиком.
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//   const newArray = firstArray.concat(secondArray);
+//   if (newArray.length > maxLength) {
+//     return newArray.slice(0, maxLength);
+//   }
+
+//   return newArray;
+// }
+
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+
+// * 17. Дополни цикл for так, чтобы он логировал все целые числа в диапазоне от start до end включительно.
+
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) {
+//   console.log(i);
+// }
+
+// * 18. Напиши функцию calculateTotal(number), которая принимает целое число (параметр number) и возвращает сумму всех целых чисел от единицы и до этого числа. Например, если number равно 3, то сумма это 1 + 2 + 3, то есть 6.
+
+// function calculateTotal(number) {
+//   let sum = 0;
+//   for (let i = 1; i <= number; i += 1) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log(calculateTotal(24));
+
+// * 19. Дополни код цикла for так, чтобы он последовательно логировал все элементы массива fruits.
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// for (let i = 0; i < fruits.length; i += 1) {
+//   const fruit = fruits[i];
+//   console.log(fruit);
+// }
+
+// * 20. Напиши функцию calculateTotalPrice(order), которая принимает один параметр order - массив чисел, и рассчитывает общую сумму его элементов. Общая сумма элементов должна сохраняться в переменной total, которая возвращается, как результат работы функции.
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   for (let i = 0; i < order.length; i += 1) {
+//     total += order[i];
+//   }
+//   return total;
+// }
+
+// console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// * 21. Напиши функцию findLongestWord(string) которая принимает произвольную строку состоящую только из слов разделённых пробелом (параметр string) и возвращает самое длинное слово в этой строке.
+
+// function findLongestWord(string) {
+//   const array = string.split(" ");
+//   let word;
+//   let wordlength = 0;
+//   for (let i = 0; i < array.length; i += 1) {
+//     if (array[i].length > wordlength) {
+//       word = array[i];
+//       wordlength = array[i].length;
+//     }
+//   }
+//   return word;
+// }
+
+// console.log(
+//   "The longest word:",
+//   findLongestWord("The quick brown fox jumped over the lazy dog")
+// );
+// console.log("The longest word:", findLongestWord("Google do a roll"));
+
+// * Найти самое большое число в массиве
+
+// const numbers = [51, 18, 13, 24, 7, 85, 19, 100];
+// let biggestNumber = numbers[0];
+// for (const number of numbers) {
+//   if (number > biggestNumber) {
+//     biggestNumber = number;
+//   }
+// }
+// console.log("biggestNumber:", biggestNumber);
